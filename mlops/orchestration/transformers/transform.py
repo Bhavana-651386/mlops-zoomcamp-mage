a7b871872cf48e1a6b903bcde8bffbeccbe8193e
Bhavana-651386 <bhavana_gollapudi@epam.com>
2024-07-02 05:44:56 +0000

    orchestration

--- mlops/orchestration/transformers/transform.py
+++ mlops/orchestration/transformers/transform.py
@@ -1,3 +1,9 @@
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import QuantileTransformer
+import pandas as pd
+
 if 'transformer' not in globals():
     from mage_ai.data_preparation.decorators import transformer
 if 'test' not in globals():
@@ -20,8 +26,28 @@ def transform(data, *args, **kwargs):
         Anything (e.g. data frame, dictionary, array, int, str, etc.)
     """
     # Specify your transformation logic here
+    imputer = SimpleImputer(strategy = 'mean')
+    data['bmi']=imputer.fit_transform(data[['bmi']])
+    encoded_data= data.copy()
 
-    return data
+    features_to_scale=['age','bmi']
+    scaler = MinMaxScaler()
+    encoded_data[features_to_scale]=scaler.fit_transform(encoded_data[features_to_scale])
+    scaler = QuantileTransformer(output_distribution='uniform')
+    encoded_data['avg_glucose_level'] = scaler.fit_transform(encoded_data[['avg_glucose_level']])
+
+    df = encoded_data.copy()
+
+    columns_to_encode = ['Residence_type', 'work_type', 'smoking_status','ever_married','gender']
+    for column in columns_to_encode:
+        encoded_column = pd.get_dummies(df[column], prefix=column)
+        df = pd.concat([df, encoded_column], axis=1)
+        df = df.drop(columns=[column],axis=1)
+
+    df = df.astype(int)
+    df.drop('id',axis=1,inplace=True)
+
+    return df
 
 
 @test
